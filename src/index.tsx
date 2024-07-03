0a2d89efa8bdcf3e36b42703b073712d82619332
githubLINGESH <linga2522004@gmail.com>
2024-07-03 23:20:38 +0530

    DOM Added to root container

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom"; // Import ReactDOM if not already imported
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "./global.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container!);
-
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
